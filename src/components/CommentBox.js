a3305144cbe12f0c09f38551d9b454added61196
Le Tan Thang <letanthang@gmail.com>
2018-10-29 06:04:47 +0700

    jest setup

---
+++ src/components/CommentBox.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class CommentBox extends Component {
+    render() {
+        return (
+            <div>Comment Box component</div>
+        );
+    }
+}
+
+export default CommentBox;
---
+++ src/components/CommentList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class CommentList extends Component {
+    render() {
+        return (
+            <div>Comment List component</div>
+        );
+    }
+}
+
+export default CommentList;
