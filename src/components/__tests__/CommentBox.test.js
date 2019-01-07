13b1f4cfcb3c658eb5f264347f821ec1eb900085
Le Tan Thang <letanthang@gmail.com>
2019-01-07 12:03:16 +0700

    commentbox test

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -4,16 +4,33 @@ import CommentBox from 'components/CommentBox'
 
 let wrapped
 
-beforeEach(()=>{
+beforeEach(() => {
     wrapped = mount(<CommentBox />)
 })
 
 it('show a textarea and a button', () => {
-    
+
     expect(wrapped.find('textarea').length).toEqual(1)
     expect(wrapped.find('button').length).toEqual(1)
+
 })
 
-afterEach(()=>{
+describe("textarea has", () => {
+    beforeEach(() => {
+        wrapped.find('textarea').simulate('change', { target: { value: 'hehe' } })
+        wrapped.update()
+    })
+    it('has a textarea that user can type in', () => {
+        expect(wrapped.find('textarea').prop('value')).toEqual('hehe')
+    })
+
+    it('when form is submited, textarea get empty', () => {
+        wrapped.find('textarea').simulate('submit')
+        expect(wrapped.find('textarea').prop('value')).toEqual('')
+    })
+})
+
+
+afterEach(() => {
     wrapped.unmount()
 })
